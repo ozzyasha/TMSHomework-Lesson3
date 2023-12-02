62d730359322cc2371c4a76654b3dd0d38b053c8
Natallia Mazur <natashamazur@bk.ru>
2023-12-02 22:05:29 +0300

    Add 8th task

--- TMSHomework-Lesson3/TMSHomework-Lesson3/Task8 - checkFlowerWatering.swift
+++ TMSHomework-Lesson3/TMSHomework-Lesson3/Task8 - checkFlowerWatering.swift
@@ -8,3 +8,7 @@
 import Foundation
 
 //    TODO: 8. Замыкание в котором ты проверяешь полил ли ты цветок, если нет то напоминание полить цветок
+
+let checkFlowerWatering = {(isWatered: Bool) -> String in
+    isWatered ? "Цветок полит" : "Нужно полить цветок"
+}
