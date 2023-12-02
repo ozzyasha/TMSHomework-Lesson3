7dbf6f4a166b58b0cca0e91964b088ba4d2c11b6
Natallia Mazur <natashamazur@bk.ru>
2023-12-03 01:10:33 +0300

    Add 9th task

--- TMSHomework-Lesson3/TMSHomework-Lesson3/Task9 - runClosureInsideClosure.swift
+++ TMSHomework-Lesson3/TMSHomework-Lesson3/Task9 - runClosureInsideClosure.swift
@@ -8,3 +8,26 @@
 import Foundation
 
 //    TODO: 9. Выполнить любое замыкание внутри замыкания по очереди/ замыкание внутри любой функции
+
+//    пример с escaping
+
+let firstClosure = {(secondClosure: @escaping (String) -> (Void)) -> Void in
+    let secondClosureMessage = "Вывод второго замыканиия"
+    secondClosure(secondClosureMessage)
+    print("Вывод первого замыкания")
+}
+
+//    второй пример с замыканием внутри функции
+
+func showStringWithFirstMessage(numberOfMessage: inout Int) {
+    
+    print("Сообщение №", numberOfMessage)
+    numberOfMessage += 1
+    
+    let showStringWithSecondMessage = {(secondNumberOfMessage: inout Int) -> Void in
+        print("Сообщение №", secondNumberOfMessage)
+    }
+    
+    showStringWithSecondMessage(&numberOfMessage)
+    
+}
